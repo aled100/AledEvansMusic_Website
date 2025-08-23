fa72a953627684ce4da916394c4fae576d9999c2
aled100 <aled100@googlemail.com>
2025-08-23 11:07:30 +0100

    update url link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // replace on deploy
-  integrations: [tailwind({applyBaseStyles: false}), sitemap()],
+  site: 'https://aledevansmusic.netlify.app', // ✅ your live Netlify URL
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    sitemap()
+  ],
 });
