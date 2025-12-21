35431736557086059b455013ee2014d5345ccea0
aled100 <aled100@googlemail.com>
2025-12-21 21:44:21 +0000

    added areas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -8,12 +7,24 @@ module.exports = {
         ivory: '#F5F1EA',
         gold: '#bda05a',
         coal: '#1d1d1b',
-        stone: '#e5dfd6'
+        stone: '#e5dfd6',
       },
       fontFamily: {
-        serif: ['"Libre Baskerville"', 'serif'],
-        sans: ['Montserrat', 'system-ui', 'sans-serif']
-      }
+        sans: [
+          'Montserrat',
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Arial',
+          'sans-serif',
+        ],
+        serif: [
+          '"Libre Baskerville"',
+          'Georgia',
+          'serif',
+        ],
+      },
     },
   },
   plugins: [],
